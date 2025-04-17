9614da3f5bc1a476bf68429b85e4b11fb04d9d9b
Isaaq Simon <IsaaqS>
2025-04-17 14:31:55 +0000

    Sphere PHP

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
   <meta name="keywords" content="Immaculata, ICD2O">
   <meta name="author" content="Isaaq Simon">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="apple-touch-icon" sizes="180x180" href="./favicon_io (11)/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="./favicon_io (11)/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="./favicon_io (11)/favicon-16x16.png">
-  <link rel="manifest" href="./favicon_io (11)/site.webmanifest">
+  <link rel="apple-touch-icon" sizes="180x180" href="./favicon_io (12)/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon_io (12)/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon_io (12)/favicon-16x16.png">
+  <link rel="manifest" href="./favicon_io (12)/site.webmanifest">
   <title>Sphere Volume Calculator (PHP)</title>
   <link rel="stylesheet" href="./css/style.css">
 </head>
